5de67b06253c0d04de1d46d69578b4ce8fdc919e
aravind <g.aravindadithya@gmail.com>
2026-01-16 20:28:26 +0000

    trainer_optimze

--- utils/trainer.py
+++ utils/trainer.py
@@ -1,9 +1,6 @@
 
 import torch
 import time
-#import model1
-import matplotlib
-matplotlib.use('TkAgg') 
 import os
 from torch.amp import autocast
 scaler = torch.amp.GradScaler('cuda')
@@ -11,16 +8,26 @@ fn_data = {}
 import wandb
 
 
-def initialize_wandb(names):
+def initialize_wandb(names, optimizer, lfn, net, num_epochs):
     
     project = names['project']
     run_name = names['name']
     id = names['run_id']
 
+    # Extracting config details
+    config = {
+        "learning_rate": optimizer.param_groups[0]['lr'],
+        "optimizer": type(optimizer).__name__,
+        "loss_function": type(lfn).__name__,
+        "epochs": num_epochs,
+        "model_architecture": type(net).__name__,
+        "weight_decay": optimizer.param_groups[0].get('weight_decay', 0),
+    }
+
     #TODO: REMOVE THIS KEY BEFORE PUSHING
     wandb.login(key ='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')
 
-    wandb.init(project=project, name=run_name, resume=True, id=id)
+    wandb.init(project=project, name=run_name, resume=True, id=id, config=config)
 
 
 
@@ -37,10 +44,10 @@ def train_network(train_loader, val_loader, test_loader, net, init_net, optimize
     print("NUMBER OF PARAMS: ", params)
 
     print("Initializing Wandb:")
-    initialize_wandb(names)
+    initialize_wandb(names, optimizer, lfn, net, num_epochs)
 
     net.cuda()
-    wandb.watch(net, log="all", log_freq=10)
+    wandb.watch(net, log="all", log_freq=100)
     #net.to(dtype=torch.float32, device='cuda')
     best_val_acc = 0
     best_test_acc = 0
@@ -56,7 +63,7 @@ def train_network(train_loader, val_loader, test_loader, net, init_net, optimize
             #net.cpu()
             d = {}
             d['state_dict'] = net.state_dict()    
-            file_path = os.path.join(root_path, f'{names['name']}_trained_nn_{i}.pth')
+            file_path = os.path.join(root_path, f'trained_nn_{i}.pth')
             torch.save(d, file_path)
             #net.cuda()
         if fn is not None:
@@ -68,40 +75,45 @@ def train_network(train_loader, val_loader, test_loader, net, init_net, optimize
             init_net.to(dtype=torch.float32, device='cpu')
 
         train_loss = train_step(net, optimizer, lfn, train_loader)
-        val_loss = val_step(net, val_loader, lfn)
-        test_loss = val_step(net, test_loader, lfn)
-        
+        val_loss = val_step(net, val_loader, lfn)       
         train_acc = get_acc_ce(net, train_loader)
-        val_acc = get_acc_ce(net, val_loader)
-        test_acc = get_acc_ce(net, test_loader)
-            
+        val_acc = get_acc_ce(net, val_loader)           
 
         if val_acc >= best_val_acc:
             best_val_acc = val_acc
-            best_test_acc = test_acc
+            best_val_loss = val_loss
             #net.cpu()
             d = {}
             d['state_dict'] = net.state_dict()
-            file_path = os.path.join(root_path, f'{names['name']}_trained_nn.pth')
+            file_path = os.path.join(root_path, 'best_model.pth')
             torch.save(d, file_path)
             #net.cuda()
-
+        '''
         if val_loss <= best_val_loss:
-            best_val_loss = val_loss
-            best_test_loss = test_loss
-
+            #best_val_loss = val_loss
+            #best_test_loss = test_loss
+            '''
+        '''
         wandb.log({
             "train/accuracy": train_acc,
             "train/loss": train_loss,
-            "test/accuracy": test_acc,
-            "test/loss": test_loss,
-            "test/best_accuracy": best_test_acc,
-            "test/best_loss": best_test_loss,
             "val/accuracy": val_acc,
             "val/loss": val_loss,
-            "val/best_accuracy": best_val_acc,
-            "val/best_loss": best_val_loss,
-        })
+        })'''
+
+    checkpoint = torch.load(os.path.join(root_path, 'best_model.pth'), weights_only=True)
+    net.load_state_dict(checkpoint['state_dict'])
+
+    best_test_loss = val_step(net, test_loader, lfn)
+    best_test_acc = get_acc_ce(net, test_loader)
+    
+    wandb.run.summary["best_test_accuracy"] = best_test_acc
+    wandb.run.summary["best_test_loss"] = best_test_loss
+    wandb.run.summary["best_val_accuracy"] = best_val_acc
+    wandb.run.summary["best_val_loss"] = best_val_loss
+    wandb.finish()
+    
+    print("FINISHED TRAINING :)")
 
     '''
 
@@ -140,9 +152,9 @@ def train_step(net, optimizer, lfn, train_loader):
         scaler.scale(loss).backward()  
         
         scaler.step(optimizer)    
-        scaler.update()                
+        scaler.update() 
 
-        # Already fixed loss accumulation:
+        wandb.log({"batch/loss": loss.item()})               
         train_loss += loss.detach().item() * len(inputs)
         
     end = time.time()
